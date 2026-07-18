07a597b2a56258de2a70813dd956494414c9c5b3
Sudharsan c <sudharsanchinnu24@gmail.com>
2026-07-18 09:36:39 +0530

    Time: 1 ms (86.55%) | Memory: 42.6 MB (86.30%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,75 +1,29 @@
-import java.util.Stack;
-
-public class Solution {
+class Solution {
     public String decodeString(String s) {
-        Stack<Integer> numStack = new Stack<>();
-        Stack<StringBuilder> strStack = new Stack<>();
-        StringBuilder currStr = new StringBuilder();
-        int currNum = 0;
+        Stack<Integer> numstack = new Stack<>();
+        Stack<StringBuilder> strstack = new Stack<>();
+        int num=0;
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()){
+            if(Character.isDigit(c)){
+                num = (num*10) + c-'0';
+            }
+            else if(c == '['){
+                 numstack.push(num);
+                 num=0;
 
-        for (char ch : s.toCharArray()) {
-            if (Character.isDigit(ch)) {
-                currNum = currNum * 10 + (ch - '0');
-            } else if (ch == '[') {
-                numStack.push(currNum);
-                strStack.push(currStr);
-                
-                currNum = 0;
-                currStr = new StringBuilder();
-            } else if (ch == ']') {
-                int repeatTimes = numStack.pop();
-                StringBuilder decodedSegment = currStr;
-                
-                currStr = strStack.pop();
-                
-                while (repeatTimes > 0) {
-                    currStr.append(decodedSegment);
-                    repeatTimes--;
-                }
-            } else {
-                currStr.append(ch);
+                 strstack.push(sb);
+                 sb = new StringBuilder();
+            }
+            else if(c == ']'){
+                  String rep=sb.toString().repeat(numstack.pop());
+                  sb = strstack.pop().append(rep);
+            }
+            else{
+                String str=String.valueOf(c);
+                sb.append(c);
             }
         }
-
-        return currStr.toString();
+        return sb.toString();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
